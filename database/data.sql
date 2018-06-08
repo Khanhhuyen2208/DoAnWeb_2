d40f3c0b6b72a2eee9a4cfe72055993dd11ae5ce
tytypham <phamthihau040596@gmail.com>
2018-06-08 21:14:24 +0700

    DB catalog_product_user

---
+++ database/data.sql
@@ -0,0 +1,73 @@
+
+
+INSERT INTO catalogs (`name`,`type_id`)
+        VALUES ('Điện thoại', 1);
+INSERT INTO catalogs (`name`,`type_id`)
+        VALUES ('Laptop', 2);
+INSERT INTO catalogs (`name`,`type_id`)
+        VALUES ('máy tính bảng',3)
+
+INSERT INTO products  (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+     VALUES ('SamSung galaxy J3', 1,3000000,3500000,100000,3600000,'Màn hình Supper AMOLED, Camera sau: 13 MP,Camera trước: 8 MP, Bộ nhớ trong: 32GB,Ram: 3GB',10,0,'SamSung',1,'2018-06-06 12:00:00','2018-06-12 12:00:00');
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+     VALUES ('SamSung galaxy J4', 1,5000000,5500000,100000,5600000,'Màn hình Supper AMOLED, Camera sau: 13 MP,Camera trước: 5 MP, Bộ nhớ trong: 16GB,Ram: 2GB',10,0,'SamSung',2,'2018-07-06 12:00:00','2018-07-12 12:00:00');
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+     VALUES ('SamSung galaxy J7 Pro', 1,6000000,6500000,100000,6600000,'Màn hình Supper AMOLED, Camera sau: 13 MP,Camera trước: 13 MP, Bộ nhớ trong: 32GB,Ram: 3GB',10,0,'SamSung',3,'2018-08-06 12:00:00','2018-08-12 12:00:00');
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+     VALUES ('Iphone 6', 1,6000000,6500000,100000,6600000,'Màn hình Supper AMOLED, Camera sau: 8 MP,Camera trước: 1.3 MP, Bộ nhớ trong: 32GB,Ram: 1 GB',10,0,'Iphone',1,'2018-06-13 12:00:00','2018-06-20 12:00:00');
+
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+     VALUES ('Iphone 7', 1,8000000,8500000,100000,8600000,'Màn hình Supper AMOLED, Camera sau: 12 MP,Camera trước: 7 MP, Bộ nhớ trong: 32GB,Ram: 4 GB',10,0,'Iphone',2,'2018-07-13 12:00:00','2018-07-20 12:00:00');
+
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+     VALUES ('Iphone 8', 1,10000000,10500000,100000,10600000,'Màn hình Supper AMOLED, Camera sau: 12 MP,Camera trước: 7 MP, Bộ nhớ trong: 64GB,Ram: 2 GB',10,0,'Iphone',2,'2018-08-13 12:00:00','2018-08-20 12:00:00');
+
+
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+     VALUES ('Oppo F5', 1,6000000,6500000,100000,6600000,'Màn hình Supper AMOLED, Camera sau: 16 MP,Camera trước: 20 MP, Bộ nhớ trong: 64GB,Ram: 6 GB',10,0,'Oppo',1,'2018-06-21 12:00:00','2018-06-28 12:00:00');
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+     VALUES ('oppo F7', 1,8000000,8500000,100000,8600000,'Màn hình Supper AMOLED, Camera sau: 16 MP,Camera trước: 25 MP, Bộ nhớ trong: 128GB,Ram: 6 GB',10,0,'oppo',2,'2018-07-21 12:00:00','2018-07-28 12:00:00');
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+     VALUES ('oppo A83', 1,10000000,10500000,100000,10600000,'Màn hình Supper AMOLED, Camera sau: 13 MP,Camera trước: 8 MP, Bộ nhớ trong: 16GB,Ram: 2 GB',10,0,'oppo',3,'2018-08-21 12:00:00','2018-06-28 12:00:00');
+
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+        VALUES ('DELL', 2,10000000,10500000,100000,10600000,'Màn hình Supper AMOLED, Camera sau: 12 MP,Camera trước: 7 MP, Bộ nhớ trong: 64GB,Ram: 2 GB',10,0,'Iphone',2,'2018-08-13 12:00:00','2018-08-20 12:00:00');
+
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+        VALUES ('ASUS', 2,10000000,10500000,100000,10600000,'Màn hình Supper AMOLED, Camera sau: 12 MP,Camera trước: 7 MP, Bộ nhớ trong: 64GB,Ram: 2 GB',10,0,'Iphone',2,'2018-08-13 12:00:00','2018-08-20 12:00:00');
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+        VALUES ('MACBOOK', 2,40000000,40500000,100000,40600000,'Màn hình Supper AMOLED, Camera sau: 12 MP,Camera trước: 7 MP, Bộ nhớ trong: 64GB,Ram: 2 GB',10,0,'Iphone',2,'2018-08-13 12:00:00','2018-08-20 12:00:00');
+
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+        VALUES ('iPad', 3,10000000,10500000,100000,10600000,'Màn hình Supper AMOLED, Camera sau: 12 MP,Camera trước: 7 MP, Bộ nhớ trong: 64GB,Ram: 2 GB',10,0,'Iphone',2,'2018-08-13 12:00:00','2018-08-20 12:00:00');
+
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+        VALUES ('ASUS SonicMaster', 3,40000000,40500000,100000,40600000,'Màn hình Supper AMOLED, Camera sau: 12 MP,Camera trước: 7 MP, Bộ nhớ trong: 64GB,Ram: 2 GB',10,0,'Iphone',2,'2018-08-13 12:00:00','2018-08-20 12:00:00');
+
+INSERT INTO products   (`name`,`catalog_id`,`price`,`starting_price`,`step_cost`,`is_auto_renew`,`content`,`discount`,`views`,`producer`,`seller_id`,`start_at`,`expire_at` )
+        VALUES (' Huawei Media pad M3', 3,10000000,10500000,100000,10600000,'Màn hình Supper AMOLED, Camera sau: 12 MP,Camera trước: 7 MP, Bộ nhớ trong: 64GB,Ram: 2 GB',10,0,'Iphone',2,'2018-08-13 12:00:00','2018-08-20 12:00:00');
+
+
+INSERT INTO users(`name`,`email`,`password`,`address`,`phone_number`,`role`) 
+        VALUES('Phạm Thị Hậu','phamthihau@gmail.com','0000','Trần đình Xu -Quận 1','01638297196',0);
+        -- 0:Admin phamthihau@gmail.com
+INSERT INTO users(`name`,`email`,`password`,`address`,`phone_number`,`role`) 
+        VALUES('Nguyễn Thị Khánh Huyền ','khanhhuyen@gmail.com','0000','Trần Hưng đạo -Quận 1','016382956789',1);
+INSERT INTO users(`name`,`email`,`password`,`address`,`phone_number`,`role`) 
+        VALUES('Min ','min@gmail.com','0000','Trần đình Xu -Quận 1','016382971900',1);
+INSERT INTO users(`name`,`email`,`password`,`address`,`phone_number`,`role`) 
+        VALUES('sun','sun@gmail.com','0000','Trần đình Xu -Quận 1','016382971900',1);
+
+        -- 1:nguoiban khanhhuyen@gmail.com
+INSERT INTO users(`name`,`email`,`password`,`address`,`phone_number`,`role`) 
+        VALUES('Phan ThịThu hiền ','thuhien@gmail.com','0000','Trần hưng đạo-Quận 1','016382123456',2);
+        -- 2:nguoimua thuhien@gmail.com
+INSERT INTO users(`name`,`email`,`password`,`address`,`phone_number`,`role`) 
+        VALUES('Nguyễn Thị Thúy hằng ','thuyhang@gmail.com','0000','Trần đình Xu -Quận 1','016382971900',2);
+INSERT INTO users(`name`,`email`,`password`,`address`,`phone_number`,`role`) 
+        VALUES('Nguyễn Đăng khương ','dangkhuong@gmail.com','0000','Trần đình Xu -Quận 1','016382971900',2);
+INSERT INTO users(`name`,`email`,`password`,`address`,`phone_number`,`role`) 
+        VALUES('Nguyễn Bá Hưng','bahung@gmail.com','0000','Trần đình Xu -Quận 1','016382971900',2);
+
+
+
