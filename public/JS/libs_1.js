ba96d10238ee5a0d841a7fed2697910dc0a5ac33
Khanhhuyen2208 <huyennguyen97.tp@gmail.com>
2018-06-19 23:50:26 +0700

    sua trang chi tiet

--- public/JS/libs_1.js
+++ public/JS/libs_1.js
@@ -3,9 +3,9 @@ function kiem_tra_dang_nhap () {
   var ten_dang_nhap, mat_khau
   ten_dang_nhap = document.getElementById('ten_dang_nhap').value
   mat_khau = document.getElementById('mat_khau').value
-  if (ten_dang_nhap == '' || mat_khau == '') {
-    alert('Kiểm tra thông tin đăng nhập')
-  } else alert('Xin chào ' + ten_dang_nhap)
+  // if (ten_dang_nhap == '' || mat_khau == '') {
+  //   alert('Kiểm tra thông tin đăng nhập')
+  // } else alert('Xin chào ' + ten_dang_nhap)
 
   // Thêm captcha cho đăng nhập
   var body = {
@@ -20,7 +20,10 @@ function kiem_tra_dang_nhap () {
     data: body,
     success: function (res) {
       console.log('ahihi')
+      window.location.href = 'http://localhost:3001/index.html'
     }
+  }).fail(function () {
+    alert('tai khoan khong hop le')
   })
 }
 // Thêm captcha cho đăng nhập
