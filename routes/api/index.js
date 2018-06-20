071309f1cc529bdc48e8998b26554c7f00ad92de
Khanhhuyen2208 <huyennguyen97.tp@gmail.com>
2018-06-20 21:58:56 +0700

    tatcasanpham

--- routes/api/index.js
+++ routes/api/index.js
@@ -20,6 +20,7 @@ router.delete('/catalog/:id', catalogController.delete)
 /* Product */
 router.get('/product', productController.index)
 router.get('/product/:id', productController.show)
+router.get('/product/:productName', productController.search)
 router.post('/product', productController.store)
 router.put('/product/:id', productController.update)
 router.delete('/product/:id', productController.delete)
